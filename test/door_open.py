b236cfc0e358f1d9b9370b30c87686e83f4fc6fa
woody1 <andrew.wood@aswood.com>
2018-03-04 17:48:23 +0000

    stepper test

--- test/door_open.py
+++ test/door_open.py
@@ -15,6 +15,21 @@ rgb = Squid(18, 23, 24)
 # A var with the LDR reader pin number
 pinldr = 14
 
+# create a default object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT()
+
+# recommended for auto-disabling motors on shutdown!
+def turnOffMotors():
+    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnOffMotors)
+
+myStepper = mh.getStepper(200, 1)  # 200 steps/rev, motor port #1
+myStepper.setSpeed(30)             # 30 RPM
+
 
 def readldr():
     ldrcount = 0 # set the count to zero
@@ -29,51 +44,16 @@ def readldr():
     return ldrcount
 
 
-# create a default object, no changes to I2C address or frequency
-mh = Adafruit_MotorHAT()
-
-# create empty threads (these will hold the stepper 1 and 2 threads)
-st1 = threading.Thread()
-st2 = threading.Thread()
-
-
-# recommended for auto-disabling motors on shutdown!
-def turnOffMotors():
-    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
-
-atexit.register(turnOffMotors)
-
-myStepper1 = mh.getStepper(200, 1)      # 200 steps/rev, motor port #1
-myStepper2 = mh.getStepper(200, 2)      # 200 steps/rev, motor port #1
-myStepper1.setSpeed(60)          # 30 RPM
-myStepper2.setSpeed(60)          # 30 RPM
-
-
-stepstyles = [Adafruit_MotorHAT.SINGLE, Adafruit_MotorHAT.DOUBLE, Adafruit_MotorHAT.INTERLEAVE, Adafruit_MotorHAT.MICROSTEP]
-
-def stepper_worker(stepper, numsteps, direction, style):
-    #print("Steppin!")
-    stepper.step(numsteps, direction, style)
-    #print("Done")
-
 while True:
     print(readldr())
     time.sleep(1) #just chill for a sec
     if readldr() > 299:
         print("Night Time - Good Night Chickens ")
-        dir = Adafruit_MotorHAT.FORWARD
-        print("forward")
+        myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.SINGLE)
+        print("Closing")
         rgb.set_color(RED)
     elif readldr() < 300 or readldr() != 0:
         print("Day Time - Go and play - Door opening")
         rgb.set_color(GREEN)
     else:
         print("BRGHT LIGHT!!!")
-
-    st1 = threading.Thread(target=stepper_worker, args=(myStepper1, dir, stepstyles[random.randint(0,3)],))
-    st1.start()
-
-    time.sleep(0.1)  # Small delay to stop from constantly polling threads (see: https://forums.adafruit.com/viewtopic.php?f=50&t=104354&p=562733#p562733)
