6ecd48c8bdcf29e4edfc2d3f8ee92ab202918ece
woody1 <andrew.wood@aswood.com>
2018-03-04 17:36:18 +0000

    testing the light sensor

--- test/door_open.py
+++ test/door_open.py
@@ -1,14 +1,33 @@
-#!/usr/bin/python
+# get the libs
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
-import time
 import atexit
 import threading
-import random
-
+import time
 from squid import *
-# Sqet the squid
+import RPi.GPIO as GPIO
+
+# Get the pins sorted
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 rgb = Squid(18, 23, 24)
+
+# A var with the LDR reader pin number
+pinldr = 14
+
+
+def readldr():
+    ldrcount = 0 # set the count to zero
+    GPIO.setup(pinldr, GPIO.OUT)
+    GPIO.output(pinldr, GPIO.LOW)
+    time.sleep(0.1) # drains the cap
+
+    GPIO.setup(pinldr, GPIO.IN) # sets teh pin to input
+    # while the input pin reas low or off or fa
+    while (GPIO.input(pinldr) == GPIO.LOW):
+        ldrcount += 1 # add 1 to the count
+    return ldrcount
+
 
 # create a default object, no changes to I2C address or frequency
 mh = Adafruit_MotorHAT()
@@ -40,39 +59,18 @@ def stepper_worker(stepper, numsteps, direction, style):
     stepper.step(numsteps, direction, style)
     #print("Done")
 
-
-while (True):
-    if not st1.isAlive():
-        randomdir = random.randint(0, 1)
-        print("Stepper 1"),
-        if (randomdir == 0):
-            dir = Adafruit_MotorHAT.FORWARD
-            print("forward"),
-
-        else:
-            dir = Adafruit_MotorHAT.BACKWARD
-            print("backward"),
-        randomsteps = random.randint(10,50)
-        print("%d steps" % randomsteps)
-        st1 = threading.Thread(target=stepper_worker, args=(myStepper1, randomsteps, dir, stepstyles[random.randint(0,3)],))
-        st1.start()
-
-    if not st2.isAlive():
-        print("Stepper 2"),
-        randomdir = random.randint(0, 1)
-        if (randomdir == 0):
-            dir = Adafruit_MotorHAT.FORWARD
-            print("forward")
-            rgb.set_color(RED)
-        else:
-            dir = Adafruit_MotorHAT.BACKWARD
-            print("backward")
-            rgb.set_color(GREEN)
-
-        randomsteps = random.randint(10,50)
-        print("%d steps" % randomsteps)
-
-        st2 = threading.Thread(target=stepper_worker, args=(myStepper2, randomsteps, dir, stepstyles[random.randint(0,3)],))
-        st2.start()
+while True:
+    print(readldr())
+    time.sleep(1) #just chill for a sec
+    if readldr() > 299:
+        print("Night Time - Good Night Chickens ")
+        dir = Adafruit_MotorHAT.FORWARD
+        print("forward")
+        rgb.set_color(RED)
+    elif readldr() < 300 or readldr() != 0:
+        print("Day Time - Go and play - Door opening")
+        rgb.set_color(GREEN)
+    else:
+        print("BRGHT LIGHT!!!")
 
     time.sleep(0.1)  # Small delay to stop from constantly polling threads (see: https://forums.adafruit.com/viewtopic.php?f=50&t=104354&p=562733#p562733)
