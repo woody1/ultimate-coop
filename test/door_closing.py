8a37fcfc9cd55fe6f204e1f656de94fe2a55bbdf
woody1 <andrew.wood@aswood.com>
2018-03-05 20:12:15 +0000

    stepper test

--- test/door_closing.py
+++ test/door_closing.py
@@ -54,7 +54,7 @@ def close_door():
         rgb.set_color(RED)
         myStepper.step(200, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.SINGLE)
         print("Closed")
-        break
+    break
 
 
 while True:
