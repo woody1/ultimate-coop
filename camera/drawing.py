a629a74f159c36a63db64243b64b0005238a6996
woody1 <andrew.wood@aswood.com>
2018-03-13 22:10:26 +0000

    added camera draw

--- camera/drawing.py
+++ camera/drawing.py
@@ -1,7 +1,16 @@
+# USAGE
+# python drawing.py
+
+# Import the necessary packages
 import numpy as np
 import cv2
 
+# Initialize our canvas as a 300x300 with 3 channels,
+# Red, Green, and Blue, with a black background
 canvas = np.zeros((300, 300, 3), dtype = "uint8")
+
+# Draw a green line from the top-left corner of our canvas
+# to the bottom-right
 green = (0, 255, 0)
 cv2.line(canvas, (0, 0), (300, 300), green)
 cv2.imshow("Canvas", canvas)
