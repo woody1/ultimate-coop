e13b0feda0cd9ce6d90420019aa3f56831138812
woody1 <andrew.wood@aswood.com>
2018-03-13 22:42:42 +0000

    added camera draw

--- camera/drawing.py
+++ camera/drawing.py
@@ -10,10 +10,7 @@ import cv2
 canvas = np.zeros((300, 300, 3), dtype = "uint8")
 
 # Let's draw one last rectangle: blue and filled in rgb - bgr
-green = (0, 255, 0)
-cv2.rectangle(canvas, (200, 50), (225, 125), green, 5)
-cv2.imshow("Canvas", canvas)
-cv2.waitKey(0)
+cv2.circle(canvas, (20, 20), 20, white)
 
 # Show our masterpiece
 cv2.imshow("Canvas", canvas)
