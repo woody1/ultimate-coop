748e6c25f733ed813841150de8d2cf1aa50cf659
woody1 <andrew.wood@aswood.com>
2018-03-13 22:23:56 +0000

    added camera draw

--- camera/drawing.py
+++ camera/drawing.py
@@ -9,10 +9,9 @@ import cv2
 # Red, Green, and Blue, with a black background
 canvas = np.zeros((300, 300, 3), dtype = "uint8")
 
-# Draw a green line from the top-left corner of our canvas
-# to the bottom-right
-green = (0, 255, 0)
-cv2.line(canvas, (0, 0), (300, 300), green)
+# Let's draw one last rectangle: blue and filled in
+blue = (255, 0, 0)
+cv2.rectangle(canvas, (200, 50), (225, 125), blue, -1)
 cv2.imshow("Canvas", canvas)
 cv2.waitKey(0)
 
