2616033f1725ca339e559a6d39e4522bb522392e
woody1 <andrew.wood@aswood.com>
2018-03-13 22:33:14 +0000

    added camera draw

--- camera/drawing.py
+++ camera/drawing.py
@@ -9,11 +9,12 @@ import cv2
 # Red, Green, and Blue, with a black background
 canvas = np.zeros((300, 300, 3), dtype = "uint8")
 
-# Let's draw one last rectangle: blue and filled in rgb - bgr
-green = (0, 255, 0)
-cv2.rectangle(canvas, (200, 50), (225, 125), green, -1)
-cv2.imshow("Canvas", canvas)
-cv2.waitKey(0)
+# Reset our canvas and draw a white circle at the center
+# of the canvas with increasing radii - from 25 pixels to
+# 150 pixels
+canvas = np.zeros((300, 300, 3), dtype = "uint8")
+(centerX, centerY) = (canvas.shape[1] // 2, canvas.shape[0] // 2)
+white = (255, 255, 255)
 
 # Show our masterpiece
 cv2.imshow("Canvas", canvas)
