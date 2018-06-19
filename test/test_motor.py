fbf679b75ea5a19b0808c1264c124bc861522256
woody1 <andrew.wood@aswood.com>
2018-06-19 15:57:53 +0100

    New changelist (2)

--- test/test_motor.py
+++ test/test_motor.py
@@ -43,9 +43,8 @@ def stepper_worker(stepper, numsteps, direction, style):
 
 while (True):
 
-    rgb.set_color(RED)
+    rgb.set_color(green)
 
     time.sleep(0.1)  # Small delay to stop from constantly polling threads (see: https://forums.adafruit.com/viewtopic.php?f=50&t=104354&p=562733#p562733)
-
 
 exit([status])
