2a2bfe54116b4e495d899b386d9b5abac39934f0
woody1 <andrew.wood@aswood.com>
2018-06-19 17:19:25 +0100

    more

--- test/test_motor.py
+++ test/test_motor.py
@@ -32,6 +32,6 @@ myStepper.setSpeed(500)             # 30 RPM
 while True:
 
     rgb.set_color(GREEN)
-    myStepper.step(myStepper, 3000, Adafruit_MotorHAT.BACKWARD,  Adafruit_MotorHAT.DOUBLE)
+    myStepper.step(3000, Adafruit_MotorHAT.BACKWARD,  Adafruit_MotorHAT.DOUBLE)
 
     time.sleep(0.2)  # Small delay to stop from constantly polling threads (see: https://forums.adafruit.com/viewtopic.php?f=50&t=104354&p=562733#p562733)
