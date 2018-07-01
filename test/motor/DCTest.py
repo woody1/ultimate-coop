306c92a4d884738b2f5d7a8b806f337713331b44
woody1 <andrew.wood@aswood.com>
2018-07-01 20:34:28 +0100

    New motor hat

--- test/motor/DCTest.py
+++ test/motor/DCTest.py
@@ -7,52 +7,56 @@ import atexit
 # create a default object, no changes to I2C address or frequency
 mh = Raspi_MotorHAT(addr=0x6f)
 
+
 # recommended for auto-disabling motors on shutdown!
-def turnOffMotors():
-	mh.getMotor(1).run(Raspi_MotorHAT.RELEASE)
-	mh.getMotor(2).run(Raspi_MotorHAT.RELEASE)
-	mh.getMotor(3).run(Raspi_MotorHAT.RELEASE)
-	mh.getMotor(4).run(Raspi_MotorHAT.RELEASE)
+def turnoffmotors():
+    mh.getMotor(1).run(Raspi_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Raspi_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Raspi_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Raspi_MotorHAT.RELEASE)
 
-atexit.register(turnOffMotors)
 
-################################# DC motor test!
+atexit.register(turnoffmotors)
+
+# DC motor test!
 myMotor = mh.getMotor(3)
 
 # set the speed to start, from 0 (off) to 255 (max speed)
 myMotor.setSpeed(150)
-myMotor.run(Raspi_MotorHAT.FORWARD);
+myMotor.run(Raspi_MotorHAT.FORWARD)
 # turn on motor
-myMotor.run(Raspi_MotorHAT.RELEASE);
+myMotor.run(Raspi_MotorHAT.RELEASE)
 
 
-while (True):
-	print "Forward! "
-	myMotor.run(Raspi_MotorHAT.FORWARD)
+while True:
+    print("Forward! ")
+    myMotor.run(Raspi_MotorHAT.FORWARD)
 
-	print "\tSpeed up..."
-	for i in range(255):
-		myMotor.setSpeed(i)
-		time.sleep(0.01)
+    print("\tSpeed up...")
+    for i in range(255):
+        myMotor.setSpeed(i)
+        time.sleep(0.01)
 
-	print "\tSlow down..."
-	for i in reversed(range(255)):
-		myMotor.setSpeed(i)
-		time.sleep(0.01)
+    print("\tSlow down...")
+    for i in reversed(range(255)):
+        myMotor.setSpeed(i)
+        time.sleep(0.01)
 
-	print "Backward! "
-	myMotor.run(Raspi_MotorHAT.BACKWARD)
+    print("Backward! ")
+    myMotor.run(Raspi_MotorHAT.BACKWARD)
 
-	print "\tSpeed up..."
-	for i in range(255):
-		myMotor.setSpeed(i)
-		time.sleep(0.01)
+    print("\tSpeed up...")
+    for i in range(255):
+        myMotor.setSpeed(i)
+        time.sleep(0.01)
 
-	print "\tSlow down..."
-	for i in reversed(range(255)):
-		myMotor.setSpeed(i)
-		time.sleep(0.01)
+    print ("\tSlow down...")
+    for i in reversed(range(255)):
+        myMotor.setSpeed(i)
+        time.sleep(0.01)
 
-	print "Release"
-	myMotor.run(Raspi_MotorHAT.RELEASE)
-	time.sleep(1.0)
+    print("Release")
+    myMotor.run(Raspi_MotorHAT.RELEASE)
+    time.sleep(1.0)
+
+
